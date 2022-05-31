bae8ddd2a546b5bfceef513041e529453262a82c
Lucas Mendoza <lucas.f.mendoza@gsk.com>
2022-05-31 11:45:26 -0300

    initiating db connection

---
+++ ContosoPizza/ContosoPizza/Data/PizzaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data;
+
+public class PizzaContext : DbContext
+{
+    public PizzaContext(DbContextOptions<PizzaContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+ 
+}
--- ContosoPizza/ContosoPizza/Program.cs
+++ ContosoPizza/ContosoPizza/Program.cs
@@ -1,3 +1,6 @@
+using ContosoPizza.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PizzaContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("PizzaDb"))
+    );
+
 
 var app = builder.Build();
 
